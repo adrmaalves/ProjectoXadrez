a0f1c4d3d9925d78bc00fc69fc0044fae2c8758f
Adriano Alves <adrianomacedo_07@hotmail.com>
2020-04-12 18:36:28 +0100

    Inicio implementação movimentos possiveis do Rei e da Torre

--- ProjectoXadrez/Xadrez/Rei.cs
+++ ProjectoXadrez/Xadrez/Rei.cs
@@ -7,6 +7,54 @@ namespace xadrez
 		{
 		}
 
+		private bool PodeMover(Posicao pos)
+		{
+			Peca p = Tab.GetPeca(pos);
+			return p == null || p.Cor != this.Cor;
+		}
+
+		public override bool[,] movimentosPossiveis()
+		{
+			bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+
+			Posicao pos = new Posicao(0, 0);
+			//Acima
+			pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna);
+			if (Tab.PosicaoValida(pos) && PodeMover(pos))
+				mat[pos.Linha, pos.Coluna] = true;
+			//NE
+			pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+			if (Tab.PosicaoValida(pos) && PodeMover(pos))
+				mat[pos.Linha, pos.Coluna] = true;
+			//Direira
+			pos.DefinirValores(Posicao.Linha, Posicao.Coluna + 1);
+			if (Tab.PosicaoValida(pos) && PodeMover(pos))
+				mat[pos.Linha, pos.Coluna] = true;
+			//SE
+			pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+			if (Tab.PosicaoValida(pos) && PodeMover(pos))
+				mat[pos.Linha, pos.Coluna] = true;
+			//Abaixo
+			pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna);
+			if (Tab.PosicaoValida(pos) && PodeMover(pos))
+				mat[pos.Linha, pos.Coluna] = true;
+			//SO
+			pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+			if (Tab.PosicaoValida(pos) && PodeMover(pos))
+				mat[pos.Linha, pos.Coluna] = true;
+			//Esquerda
+			pos.DefinirValores(Posicao.Linha, Posicao.Coluna - 1);
+			if (Tab.PosicaoValida(pos) && PodeMover(pos))
+				mat[pos.Linha, pos.Coluna] = true;
+			//NO
+			pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+			if (Tab.PosicaoValida(pos) && PodeMover(pos))
+				mat[pos.Linha, pos.Coluna] = true;
+
+
+			return mat;
+		}
+
 		public override string ToString()
 		{
 			return "R";
--- ProjectoXadrez/Xadrez/Torre.cs
+++ ProjectoXadrez/Xadrez/Torre.cs
@@ -8,6 +8,60 @@ namespace xadrez
 		{
 		}
 
+		private bool PodeMover(Posicao pos)
+		{
+			Peca p = Tab.GetPeca(pos);
+			return p == null || p.Cor != this.Cor;
+		}
+
+		public override bool[,] movimentosPossiveis()
+		{
+			bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+
+			Posicao pos = new Posicao(0, 0);
+			//Acima
+			pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna);
+			while (Tab.PosicaoValida(pos) && PodeMover(pos))
+			{
+				mat[pos.Linha, pos.Coluna] = true;
+				if (Tab.GetPeca(pos) != null && Tab.GetPeca(pos).Cor != Cor)
+					break;
+				pos.Linha = pos.Linha - 1;
+			}
+
+			//Abaixo
+			pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna);
+			while (Tab.PosicaoValida(pos) && PodeMover(pos))
+			{
+				mat[pos.Linha, pos.Coluna] = true;
+				if (Tab.GetPeca(pos) != null && Tab.GetPeca(pos).Cor != Cor)
+					break;
+				pos.Linha = pos.Linha + 1;
+			}
+
+			//Esquerda
+			pos.DefinirValores(Posicao.Linha, Posicao.Coluna - 1);
+			while (Tab.PosicaoValida(pos) && PodeMover(pos))
+			{
+				mat[pos.Linha, pos.Coluna] = true;
+				if (Tab.GetPeca(pos) != null && Tab.GetPeca(pos).Cor != Cor)
+					break;
+				pos.Coluna = pos.Coluna - 1;
+			}
+
+			//Direita
+			pos.DefinirValores(Posicao.Linha, Posicao.Coluna + 1);
+			while (Tab.PosicaoValida(pos) && PodeMover(pos))
+			{
+				mat[pos.Linha, pos.Coluna] = true;
+				if (Tab.GetPeca(pos) != null && Tab.GetPeca(pos).Cor != Cor)
+					break;
+				pos.Coluna = pos.Coluna + 1;
+			}
+
+			return mat;
+		}
+
 		public override string ToString()
 		{
 			return "T";
