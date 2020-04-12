28e42df898e7cefff430b98f340495e8387e73e3
Adriano Alves <adrianomacedo_07@hotmail.com>
2020-04-12 12:37:05 +0100

    Tratamento de erros com excepção personalizada

--- ProjectoXadrez/Program.cs
+++ ProjectoXadrez/Program.cs
@@ -8,17 +8,30 @@ namespace ProjectoXadrez
 	{
 		static void Main(string[] args)
 		{
-			Tabuleiro tab = new Tabuleiro(8, 8);
+			try
+			{
+				Tabuleiro tab = new Tabuleiro(8, 8);
 
 
-			tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-			tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-			tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
+				tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
+				tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 9));
+				tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
+				tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 2));
 
-
-			Tela.imprimirTabuleiro(tab);
-
-			Console.ReadKey(true);
+				Tela.imprimirTabuleiro(tab);
+			}
+			catch (TabuleiroException t)
+			{
+				Console.WriteLine(t.Message);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Unexpected Error: " + e.Message);
+			}
+			finally
+			{
+				Console.ReadKey(true);
+			}
 		}
 	}
 }
